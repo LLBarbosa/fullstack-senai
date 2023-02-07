f30fc59b9cdf21a2e726e27763502fcde3ad7acb
Laercio Luiz Barbosa <llbarbosa72@hotmail.com>
2023-02-07 14:59:17 -0300

    Created new paste

---
+++ [M1S2]-ex-01/script.js
@@ -0,0 +1,56 @@
+var aluno = [{
+
+    nome: "Marcelo Vieira",
+    altura: 1.75,
+    idade: 25,
+    sexo: "Masculino",
+    sala: 101,
+    matematica: 7.5
+
+},
+
+{
+    nome: "Cristiano Amaral",
+    altura: 1.82,
+    idade: 50,
+    sexo: "Masculino",
+    sala: 202,
+    matematica: 5.9
+
+},
+
+{
+    nome: "Cristina Vicenzo",
+    altura: 1.62,
+    idade: 34,
+    sexo: "Feminino",
+    sala: 201,
+    matematica: 9.8
+},
+
+{
+    nome: "Pedro Siqueira",
+    altura: 1.71,
+    idade: 37,
+    sexo: "Masculino",
+    sala: 301,
+    matematica: 6.3
+},
+
+{
+    nome: "Sarita do Amaral",
+    altura: 1.58,
+    idade: 45,
+    sexo: "Feminino",
+    sala: 202,
+    matematica: 8.5
+}];
+
+var media = 0
+for (i = 0; i < aluno.length; i++) {
+    media += aluno[i].matematica;
+
+};
+
+media = media / aluno.length;
+console.log("A média dos alunos é " + media);
