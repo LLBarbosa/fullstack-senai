0c173021c516ea698d9540864e6df7d86441a8eb
Laercio Luiz Barbosa <llbarbosa72@hotmail.com>
2023-02-09 14:26:14 -0300

    exercise [M1S02]-ex-6 - update format

--- [M1S02]-ex-6/script.js
+++ [M1S02]-ex-6/script.js
@@ -2,23 +2,13 @@ const number = parseInt(prompt("Digite um número inteiro entre 1 - 10: "));
 
 if (number > 0 && number <= 10) {
 
-console.log("Tabuada de " + number +":");
-for (i = 1; i <= 10; i++) {
-       let calcule = number * i;
-       console.log(number + " x " + i + " = " + calcule)
-};
-
+       console.log("Tabuada de " + number + ":");
+       for (i = 1; i <= 10; i++) {
+              let calcule = number * i;
+              console.log(number + " x " + i + " = " + calcule)
+       };
 
 } else {
        console.log("Número não permitido!!");
 }
-
-
-
-
-
-
-
-
-
 
