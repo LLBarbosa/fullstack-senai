7cfe7107256304deed77ac97701412fde817d705
Laercio Luiz Barbosa <llbarbosa72@hotmail.com>
2023-02-10 11:15:42 -0300

    exercise [M1S02]-ex-5

---
+++ [M1S02]-ex-5/script.js
@@ -0,0 +1,19 @@
+
+         const startButton = document.querySelector("#start");
+         const timerDisplay = document.querySelector("#timer");
+     
+         startButton.addEventListener("click", function () {
+             let count = 10;
+             timerDisplay.innerHTML = count;
+             const countDown = setInterval(function () {
+                 count--;
+                 if (count === 0) {
+                     timerDisplay.innerHTML = "Seu tempo acabou!! Tente novamente!!";
+                     clearInterval(countDown);
+                 } else {
+                     timerDisplay.innerHTML = count;
+                 }
+             }, 1000);
+         });
+        
+        
