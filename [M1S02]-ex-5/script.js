c68e8ff6fa4837c5913c69bdb075d8d4332d7fc1
Laercio Luiz Barbosa <llbarbosa72@hotmail.com>
2023-02-12 20:38:26 -0300

    add comments

--- [M1S02]-ex-5/script.js
+++ [M1S02]-ex-5/script.js
@@ -1,19 +1,18 @@
 
-         const startButton = document.querySelector("#start");
-         const timerDisplay = document.querySelector("#timer");
-     
-         startButton.addEventListener("click", function () {
-             let count = 10;
-             timerDisplay.innerHTML = count;
-             const countDown = setInterval(function () {
-                 count--;
-                 if (count === 0) {
-                     timerDisplay.innerHTML = "Seu tempo acabou!! Tente novamente!!";
-                     clearInterval(countDown);
-                 } else {
-                     timerDisplay.innerHTML = count;
-                 }
-             }, 1000);
-         });
-        
-        
+const startButton = document.querySelector("#start");
+const timerDisplay = document.querySelector("#timer");
+
+startButton.addEventListener("click", function () {
+    let count = 10;
+    timerDisplay.innerHTML = count;
+    const countDown = setInterval(function () {
+        count--;
+        if (count === 0) {
+            timerDisplay.innerHTML = "Seu tempo acabou!! Tente novamente!!";
+            clearInterval(countDown);
+        } else {
+            timerDisplay.innerHTML = count;
+        }
+    }, 1000);
+});
+
