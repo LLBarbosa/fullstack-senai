1eb6eeb53b859d8d4f7d5e3a29d0eb63552ae63b
Laercio Luiz Barbosa <llbarbosa72@hotmail.com>
2023-02-14 16:06:27 -0300

    update exercises

---
+++ [M1S02]-ex-3/script.js
@@ -0,0 +1,8 @@
+    
+function login () {
+    let username = document.getElementById("username");
+    let password = document.getElementById("password");
+    
+    console.log(username.value);
+    console.log(password.value);
+}
--- [M1S02]-ex-8/script.js
+++ [M1S02]-ex-8/script.js
@@ -1,30 +1,25 @@
-let count = 1;
-let total = 0;
-let change = 0
-
 //console.log("Lojas Tabajara");
 function cashRegister() {
-console.log("Lojas Tabajara");  
-while (price != 0) {
-    var price = parseFloat(prompt(`Produto ${count}`));
-    console.log(`Produto ${count}: R$ ${price.toFixed(2)}`);
-    count++;
-    total += price;
-
+    console.log("Lojas Tabajara");
+    let count = 1;
+    let total = 0;
+    let change = 0;
+    let price;
+    while (price !== 0) {
+        price = parseFloat(prompt(`Produto ${count}`));
+        console.log(`Produto ${count}: R$ ${price.toFixed(2)}`);
+        count++;
+        total += price;
+    }
+    console.log(`Total: R$ ${total.toFixed(2)}`);
+    let money = parseFloat(prompt(`Dinheiro: R$ `));
+    console.log(`Dinheiro: R$ ${money.toFixed(2)}`);
+    change = money - total;
+    console.log(`Troco: R$ ${change.toFixed(2)}\n\n`);
 }
 
+while (true) cashRegister();
 
-
-console.log(`Total: R$ ${total.toFixed(2)}`);
-var money = parseFloat(prompt(`Dinheiro: R$ `));
-console.log(`Dinheiro: R$ ${money.toFixed(2)}`)
-change = money - total;
-console.log(`Troco: R$ ${change.toFixed(2)} `);
-
-}
-
-cashRegister();
-    
 
 
 
