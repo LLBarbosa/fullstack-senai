ea40252c03dac09b8667e06f3010efeb0f1c5813
Laercio Luiz Barbosa <llbarbosa72@hotmail.com>
2023-02-14 09:13:01 -0300

    add exercise [M1S02]-ex-8

---
+++ [M1S02]-ex-8/script.js
@@ -0,0 +1,80 @@
+let count = 1;
+let total = 0;
+let change = 0
+
+console.log("Lojas Tabajara");
+function cashRegister() {
+while (price != 0) {
+    var price = parseFloat(prompt(`Produto ${count}`));
+    console.log(`Produto ${count}: R$ ${price.toFixed(2)}`);
+    count++;
+    total += price;
+
+}
+
+
+
+console.log(`Total: R$ ${total.toFixed(2)}`);
+var money = parseFloat(prompt(`Dinheiro: R$ `));
+console.log(`Dinheiro: R$ ${money.toFixed(2)}`)
+change = money - total;
+console.log(`Troco: R$ ${change.toFixed(2)} `);
+
+}
+
+
+cashRegister();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
