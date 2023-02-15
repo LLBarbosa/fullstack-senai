cf0a32b11d969c7f151f0158cfc325c519b9252b
Laercio Luiz Barbosa <llbarbosa72@hotmail.com>
2023-02-15 10:58:06 -0300

    update exercise

--- [M1S03}-ex-3/script.js
+++ [M1S03}-ex-3/script.js
@@ -1,9 +1,10 @@
 console.log('Hello');
 
-let dailyTasks = ["Banho", "Dar comida para o cachorro", "Trabalho", "Atvidade Física", "Sair com os amigos", "Fazer compras"];
+let dailyTasks = ["Banho", "Dar comida para o cachorro", "Trabalho", "Atividade Física", "Sair com os amigos", "Fazer compras"];
 let task;
 
-
+//1.Busca a tarefa na lista
+//------------------------
 function lookForTasks(task) {
   task = dailyTasks.find(function (element) {
     return element === task;
@@ -15,38 +16,38 @@ function lookForTasks(task) {
     return "A tarefa não foi encontrada na lista.";
   }
 }
-
+//2.Altera uma tarefa trocando por outra 
+//-------------------------------------
 function changeTask(oldTask, newTask) {
   let index = dailyTasks.indexOf(oldTask);
   if (index != -1) {
     dailyTasks[index] = newTask
   }
-
 }
-
+//3.Deleta uma tarefa específica
+//-----------------------------
 function deleteTask(task) {
   return dailyTasks.filter(function (element) {
     return element !== task;
 
   })
-
 }
-
+//4.Adiciona uma tarefa
+//--------------------
 function addTasks(task) {
   dailyTasks.push(task);
   console.log(dailyTasks);
 
 }
 
- //console.log(lookForTasks("Trabalho"));
+//TESTES DE IMPLEMENTAÇÃO:
+//------------------------
 
-//changeTask("Banho", "Carro");
+// console.log(lookForTasks("Trabalho"));
+
+//changeTask("Banho", "Comer");
 //console.log(dailyTasks);
 
 //let newDailyTasks = console.log(deleteTask("Trabalho"));
 
 //console.log(addTasks("Comer"));
-
-
-
-
