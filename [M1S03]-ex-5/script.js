8d3c44ab1b863d193378cb8dba3bae72d2e026ef
Laercio Luiz Barbosa <llbarbosa72@hotmail.com>
2023-02-20 18:31:49 -0300

    update exercises

--- [M1S03]-ex-5/script.js
+++ [M1S03]-ex-5/script.js
@@ -1,13 +1,24 @@
-fetch('https://api.github.com/users/LLBarbosa')
-        .then(response => response.json())
-        .then(data => {
-          // Preenche os elementos HTML com as informações obtidas
-          document.querySelector('.name').textContent = data.name;
-          document.querySelector('.location').textContent = data.location;
-          document.querySelector('.bio').textContent = data.bio;
-          document.querySelector('.followers').textContent = data.followers;
-          document.querySelector('.following').textContent = data.following;
-          document.querySelector('.public-repos').textContent = data.public_repos;
-          document.querySelector('img').src = data.avatar_url;
-        })
-        .catch(error => console.error(error));
+const apiUrl = 'https://api.github.com/users/LLBarbosa';
+
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    const profileCard = document.querySelector('.card');
+    const avatar = profileCard.querySelector('.avatar_url');
+    const name = profileCard.querySelector('.name');
+    const login = profileCard.querySelector('.login');
+    const bio = profileCard.querySelector('.bio');
+    const location = profileCard.querySelector('.location');
+    const followers = profileCard.querySelector('.followers');
+    const following = profileCard.querySelector('.following');
+    const repos = profileCard.querySelector('.public_repos');
+
+    avatar.src = data.avatar_url;
+    name.textContent = data.name;
+    bio.textContent = data.bio;
+    login.textContent = "@" + data.login;
+    location.textContent = data.location;
+    followers.textContent = data.followers;
+    following.textContent = data.following;
+    repos.textContent = data.public_repos;
+  });
